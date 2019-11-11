cf21f0a13f0b833de5ec6e943a753e1fb80c3780
sofbrin <sofbrin@hotmail.com>
2019-11-11 09:27:59 +0100

    test

--- gpbapp/views.py
+++ gpbapp/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 from gpbapp.grandpyAnswer import GrandpyAnswer
 
--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
 API_KEY = os.getenv('API_KEY')
